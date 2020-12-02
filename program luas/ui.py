08dc218d65d0613b597907c3e3b783bebea0c221
MFurqon2710 <57359241+MFurqon2710@users.noreply.github.com>
2020-12-02 23:45:43 +0700

    Add files via upload

--- program luas/ui.py
+++ program luas/ui.py
@@ -2,22 +2,45 @@ import luas
 def jeda():
     print("--------------------------------------------")
     pass
+def lanjut():
+    status = 1
+    while (status==1):
+        jawaban =input("lanjut ? (y/n) :  ")
+        if (jawaban=='y'):
+            power = True
+            status =-1
+            pass
+        elif (jawaban=='n'):
+            power = False;
+            status =-1
+            pass
+        else:
+            print("input salah")
 print("Tugas Akhir Pemrosesan Paralel")
 jeda()
-print("Selamat datang di program mencari luas, luas apa yang ingin anda cari ?")
-print("1. Luas Segitiga")
-print("2. Luas Lingkaran")
-print("3. Luas Persegi")
-opsi = int(input("Masukkan nomor dari fungsi yang anda jalankan : "))
-if (opsi==1):
-    luas.segitiga()
-    pass
-elif (opsi==2):
-    luas.lingkaran()
-    pass
-elif (opsi==3):
-    luas.persegi
-    pass
-else:
-    print("Maaf tidak ada perintah yang tersedia dengan angka yang anda masukkan")
-    pass
+power = True
+while (power==True):
+    print("Selamat datang di program mencari luas, luas apa yang ingin anda cari ?")
+    print("1. Luas Segitiga")
+    print("2. Luas Lingkaran")
+    print("3. Luas Persegi")
+    print("4. Keluar ")
+    opsi = int(input("Masukkan nomor dari fungsi yang anda jalankan : "))
+    if (opsi==1):
+        luas.segitiga()
+        lanjut()
+        pass
+    elif (opsi==2):
+        luas.lingkaran()
+        lanjut()
+        pass
+    elif (opsi==3):
+        luas.persegi
+        lanjut()
+        pass
+    elif (opsi==4):
+        power = False
+        pass
+    else:
+        print("Maaf tidak ada perintah yang tersedia dengan angka yang anda masukkan")
+        pass
